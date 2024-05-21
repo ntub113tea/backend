7d26bf8a5bf9247313e87ab6cf85e20a1c162944
jia-kai <11046006@ntub.edu.tw>
2024-05-21 16:37:29 +0800

    修改 電話樣式

--- myapp/templatetags/my_filters.py
+++ myapp/templatetags/my_filters.py
@@ -5,5 +5,5 @@ register = template.Library()
 @register.filter
 def mask_phone(phone):
     if phone and len(phone) > 3:
-        return phone[:-3] + 'xxx'
+        return 'xxxx-'+phone[4:7] +'-'+phone[7:10] 
     return phone
